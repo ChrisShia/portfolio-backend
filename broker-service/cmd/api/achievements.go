508406a10c06c06acb1a02c3d02fd8be681c0436
ChrisShia <christos.shiakas@gmail.com>
2025-10-23 07:54:12 +0300

    main:
    In achievements.go added IconSvg field in anonymous struct of achievements var.
    In svgconstants.go, collected all svg icon constants.

--- broker-service/cmd/api/achievements.go
+++ broker-service/cmd/api/achievements.go
@@ -14,12 +14,13 @@ func (app *App) achievements(w http.ResponseWriter, r *http.Request) {
 var achievements = []struct {
 	Year    string `json:"year,omitempty"`
 	Title   string `json:"title"`
+	IconSvg string `json:"icon"`
 	Content string `json:"content,omitempty"`
 }{
-	{"2022", "Software Developer Albourne", "Say a few words for the projects and technologies"},
-	{"2021", "Physics Degree Msc", "Some words for Physics Msc"},
-	{"2017", "Cern Internship", "Some words for Cern"},
-	{"2013", "Physics Degree Bsc", "Thesis on Thermodynamic."},
+	{"2022", "Java Software Developer", softwareIconSvg, "Say a few words for the projects and technologies"},
+	{"2021", "Physics Degree Msc", emcIconSvg, "Some words for Physics Msc"},
+	{"2017", "Cern Internship", emcIconSvg, "Some words for Cern"},
+	{"2013", "Physics Degree Bsc", emcIconSvg, "Thesis on Thermodynamic."},
 	//{"Physics Undergraduate Degree", "University of Cyprus", "2013", "Thesis on Thermodynamic of Black Holes."},
 	//{"Physics Undergraduate Degree", "University of Cyprus", "2013", "Thesis on Thermodynamic of Black Holes."},
 }
